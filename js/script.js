ddd7dd2fccc8a8e5dc9ccb5e32e47041f017e673
Boffin-ux <fnamily@mail.ru>
2021-06-12 10:47:48 +0300

    Выполнил усложненное задание Урок №14

--- js/script.js
+++ js/script.js
@@ -12,10 +12,12 @@ document.addEventListener('DOMContentLoaded', function () {
       if (this.selector[0] === '.') {
          const div = document.createElement('div');
          this.createCssText(div);
+         this.eventsKeydown(div);
          document.body.append(div);
       } else if (this.selector[0] === '#') {
          const p = document.createElement('p');
          this.createCssText(p);
+         this.eventsKeydown(p);
          document.body.append(p);
       } else {
          alert('Ошибка ввода');
@@ -33,7 +35,42 @@ document.addEventListener('DOMContentLoaded', function () {
       selectorBlock.style.background = this.bg;
       selectorBlock.style.fontSize = `${this.fontSize}px`;
       selectorBlock.style.position = 'absolute';
-   }
+      // selectorBlock.style.top = '0';
+      // selectorBlock.style.left = '0';
+   };
+
+   DomElement.prototype.eventsKeydown = function (selector) {
+      selector.style.top = 0;
+      selector.style.left = 0;
+      let bottomSum = 0,
+         topSum = 0,
+         leftSum = 0,
+         rightSum = 0;
+      document.addEventListener('keydown', function (event) {
+         if (event.code === 'ArrowUp') {
+            bottomSum += 10;
+            topSum -= 10;
+            selector.style.top = `${topSum}px`;
+            selector.style.bottom = `${bottomSum}px`;
+         } else if (event.code === 'ArrowDown') {
+            bottomSum -= 10;
+            topSum += 10;
+            selector.style.top = `${topSum}px`;
+            selector.style.bottom = `${bottomSum}px`;
+         } else if (event.code === 'ArrowRight') {
+            leftSum += 10;
+            rightSum -= 10;
+            selector.style.right = `${rightSum}px`;
+            selector.style.left = `${leftSum}px`;
+         } else if (event.code === 'ArrowLeft') {
+            leftSum -= 10;
+            rightSum += 10;
+            selector.style.right = `${rightSum}px`;
+            selector.style.left = `${leftSum}px`;
+         }
+      });
+   };
+
    const domElement = new DomElement();
    domElement.addElement();
 
